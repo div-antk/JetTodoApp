cf8c757899eaabc1bd01ddbfefd4bb7d49e00e3a
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-24 23:59:00 +0900

    CRUDメソッドの作成

---
+++ app/src/main/java/com/example/jettodoapp/TaskDao.kt
@@ -0,0 +1,22 @@
+package com.example.jettodoapp
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insertTask(task: Task)
+
+    @Query("SELECT * FROM Task")
+    fun loadAllTasks(): Flow<List<Task>>
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+}
