2b052af30904cd114f16e773240dc53e782364d8
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-28 23:59:00 +0900

    不要な記述の削除、Gradleの修正

--- app/src/main/java/com/example/jettodoapp/TaskDao.kt
+++ app/src/main/java/com/example/jettodoapp/TaskDao.kt
@@ -1,10 +1,11 @@
 package com.example.jettodoapp
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+//import androidx.room.Dao
+//import androidx.room.Delete
+//import androidx.room.Insert
+//import androidx.room.Query
+//import androidx.room.Update
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
