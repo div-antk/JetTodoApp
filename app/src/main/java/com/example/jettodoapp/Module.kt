5729bd8b42e178ef97f9e2f51b73ebd5ea598f68
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-29 23:59:00 +0900

    Hiltモジュールの作成

--- app/src/main/java/com/example/jettodoapp/Module.kt
+++ app/src/main/java/com/example/jettodoapp/Module.kt
@@ -1,5 +1,23 @@
 package com.example.jettodoapp
 
-class Module {
+import android.content.Context
+import androidx.room.Room
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
+@dagger.Module
+@InstallIn(SingletonComponent::class)
+class Module {
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, AppDatabase::class.java, "task_database").build()
+
+    @Singleton
+    @Provides
+    fun provideDao(db: AppDatabase) = db.taskDao()
 }
