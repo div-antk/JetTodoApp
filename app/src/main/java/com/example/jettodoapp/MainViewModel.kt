2020331cebb1d196a6b1c8be224579d930d44c7b
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-31 00:52:26 +0900

    MainViewModelベースの作成

---
+++ app/src/main/java/com/example/jettodoapp/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.jettodoapp
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
+    
+}
